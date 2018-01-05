79d97bedc4faa731c5625c8e0d97efd42a8532d0
Roger Lu <rogerfrontend@gmail.com>
2018-01-05 01:56:15 -0500

    54.Deisgn Pages Layouts

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,22 +27,30 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [200, 300]
+  # process resize_to_fit: [400, 600]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+     process resize_to_fit: [50, 50]
+   end
+   version :small do
+     process resize_to_fit: [250, 250]
+
+   end
+   version :medium do
+     process resize_to_fit: [550, 550]
+
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
